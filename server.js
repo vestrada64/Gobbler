c1ced1fe93a535831fdd5a4b8c58c4f8e2d383df
Vanessa Estrada <vestrada64@gmail.com>
2018-02-02 14:50:18 -0800

    fix config syntax

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('../config/database');
+require('./config/database');
 var methodOverride = require('method-override');
 
 var index = require('./routes/index');
