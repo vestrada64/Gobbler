bd6620563b6f0c44edc5c6f8e756b6b1efbed400
Nick Taylor <nicholasjt22@gmail.com>
2018-02-06 17:12:12 -0800

    create gobble

--- controllers/api/gobbles.js
+++ controllers/api/gobbles.js
@@ -5,17 +5,14 @@ var Gobble = require('../../models/Gobble');
 
 function createGobble(req, res) {
     console.log(req.body);
-
     var gobble = new Gobble(req.body);
     gobble.save(function(err) {
         if (err) console.log(err);
-        req.user.gobbles.push(gobble);
+        req.user.gobbles.push(gobble._id);
         req.user.save(function(err) {
-            res.json(req.user).status(200);
+            res.status(201).json(req.user);
         })
-
     })
-
 }
 
 function deleteGobble(req, res) {
