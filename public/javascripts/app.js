623960869e66e9a9dcb625b8540d421f250bcaf4
Noel Abellera <noel@Noels-MacBook-Pro.local>
2018-02-06 19:40:21 -0800

    reject my request changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,6 @@
 
+var delBtn = document.getElementById('deleteBtn');
+
 function gobble() {
     console.log(user.populate('gobbles', function(err) {console.log(JSON.stringify(user, null, 2))}));
     if( !$('#gobble').val() ) return;
@@ -13,3 +15,9 @@ function gobble() {
         $('#gobble').val(JSON.stringify(data, null, 2));
     });
 }
+
+delBtn.addEventListener('click', function(evt) {
+    fetch(`/api/gobbles/${evt.target.dataset.id}`, {
+        method: 'DELETE'
+    })
+});
